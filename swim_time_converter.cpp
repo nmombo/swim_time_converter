1af5b4d3fba1e03406052f0aa5e46350e48a579f
Noah Momblanco <nmombo@umich.edu>
2017-03-21 15:24:24 -0400

    Initial upload

---
+++ swim_time_converter.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+void main() 
+{
+	// Receive input data
+	cout << "This program can convert swiming times between SCY, SCM, and LCM." << endl;
+	string length;
+	cin << "What is the length of the event? Give response in form \"distance, pool length\", for example \"100 SCY\". " << length;
+	string time;
+	cin << "What is the time? Give response in form \"MM:SS.DD\", for example \"04:25.36\". " << time;
+
+	// Perfrom calculations
+
+	// Output results
+}
